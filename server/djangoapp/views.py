efce979bafd939d726264bb91d5b1e3ebdc1e414
Aleksandar Marinkovic <lxmarinkovic@gmail.com>
2021-04-16 09:27:33 +0200

    Minor changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,7 +1,8 @@
+""" Djangoapp Views"""
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, redirect
 # from .models import related models
 # from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
@@ -19,89 +20,83 @@ logger = logging.getLogger(__name__)
 
 # Create an `about` view to render a static about page
 def about(request):
+    """ About View"""
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/about.html', context)
 
-
 # Create a `contact` view to return a static contact page
+
+
 def contact(request):
+    """ Contact View """
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
+
+
 def login_request(request):
+    """ Login Request """
     context = {}
-   
     if request.method == "POST":
-        
         username = request.POST['username']
-        password = request.POST['passw']
-        
+        password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
-            
             login(request, user)
             return render(request, 'djangoapp/index.html', context)
-        else:
-           
+       
             return render(request, 'djangoapp/index.html', context)
     else:
-        return render(request, 'djangoapp/index.html', context)
+        return render(request, 'djangoapp/index.html', context) 
 
 # Create a `logout_request` view to handle sign out request
+
+
 def logout_request(request):
+    """ Logout Request """
     context = {}
-  
-    print("Log out the user `{}`".format(request.user.username))
-   
+    print("User logged out `{}`".format(request.user.username))
     logout(request)
-    
     return render(request, 'djangoapp/index.html', context)
 
-
 # Create a `registration_request` view to handle sign up request
+
+
 def registration_request(request):
+    """ Registration Request """
     context = {}
-    
     if request.method == 'GET':
         return render(request, 'djangoapp/registration.html', context)
-   
     elif request.method == 'POST':
-   
         username = request.POST['username']
-        password = request.POST['passw']
+        password = request.POST['password']
         first_name = request.POST['firstname']
         last_name = request.POST['lastname']
         user_exist = False
         try:
-        
             User.objects.get(username=username)
             user_exist = True
         except:
-    
             logger.debug("{} is new user".format(username))
-       
         if not user_exist:
-     
-            user = User.objects.create_user(username=username, 
-                                            first_name=first_name, 
-                                            last_name=last_name,
-                                            password=password)
-           
+            user = User.objects.create_user(
+                username=username, first_name=first_name, last_name=last_name, password=password)
             login(request, user)
             return render(request, 'djangoapp/index.html', context)
         else:
             return render(request, 'djangoapp/index.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+
+
 def get_dealerships(request):
+    """ Dealerships View """
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-
-
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
@@ -109,4 +104,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
