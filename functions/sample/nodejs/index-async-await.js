418e26aee4ce50a018da7f8602a66389febe00df
JChBrandt <59009010+JChBrandt@users.noreply.github.com>
2021-09-06 16:36:24 +0200

    Update index-async-await.js

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -10,13 +10,41 @@
          url: params.COUCH_URL,
          plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
      });
- 
- 
-     try {
-         let dbList = await cloudant.db.list();
-         return { "dbs": dbList };
-     } catch (error) {
-         return { error: error.description };
+
+     let selector = {};
+     if (params.state) {
+         selector.st = { '$eq': params.state };
      }
- 
+
+     const dbListPromise = await getDbs(cloudant, selector);
+     return dbListPromise;
+ }
+
+ function getDbs(cloudant, selector={}) {
+     return new Promise((resolve, reject) => {
+         const db = cloudant.use('dealerships');
+         db.find({ selector, use_index: '_design/st' })
+         .then(result => {
+             resolve({ dealerships: formatData(result) })
+         })
+         .catch(err => {
+             reject({ err: err});
+         });
+     });
+ }
+
+  function formatData(result) {
+     return result.docs.map((row) => {
+         const doc = row;
+         return {
+             "id": doc.id,
+             "city": doc.city,
+             "state": doc.state,
+             "st": doc.st,
+             "address": doc.address,
+             "zip": doc.zip,
+             "lat": doc.lat,
+             "long": doc.long
+         }
+     })
  }
