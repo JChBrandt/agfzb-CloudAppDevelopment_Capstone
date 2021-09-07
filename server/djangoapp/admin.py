b6c737d2366bd895b0e0acf27a626baa1a8f6aa1
JChBrandt <59009010+JChBrandt@users.noreply.github.com>
2021-09-07 17:31:37 +0200

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
-
-# CarModelInline class
+from .models import CarMake, CarModel
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['maker', 'name','modeltype','engine','year']
+
+# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name','description']
+    inlines = [CarModelInline]
 
-# Register models here
+# Register your models here.
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
