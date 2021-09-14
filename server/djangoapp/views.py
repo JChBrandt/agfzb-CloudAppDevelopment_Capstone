3401440a53a0c25021dacc61114b433e62ad630a
JChBrandt <59009010+JChBrandt@users.noreply.github.com>
2021-09-14 17:03:35 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
+from .local_settings import API_URL_DEALERSHIPS
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -15,7 +16,6 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
 
 # Create an `about` view to render a static about page
 def about(request):
@@ -95,38 +95,49 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
-# If it is a POST request
-    elif request.method == 'POST':
-        # Get user information from request.POST
-        username = request.POST['username']
-        password = request.POST['psw']
-        first_name = request.POST['firstname']
-        last_name = request.POST['lastname']
-        user_exist = False
-        try:
-            # Check if user already exists
-            User.objects.get(username=username)
-            user_exist = True
-        except:
-            # If not, simply log this is a new user
-            logger.debug("{} is new user".format(username))
-        # If it is a new user
-        if not user_exist:
-            # Create user in auth_user table
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                            password=passwor)
-            # Login the user and redirect to course list page
-            login(request, user)
-            # redirect to course list page
-            return redirect('djangoapp:index')
-        else:
-            return render(request, 'djangoapp/registration.html', context)
+        url = API_URL_DEALERSHIPS
+        dealerships = get_dealers_from_cf(url)
+        context = {"dealerships": dealerships}
+        get_dealerships_view = render(request, 'djangoapp/index.html', context)
+    return get_dealerships_view
+
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_reviews(request, dealer_id, dealer_name):
+    """ Dealerships Details """
+    if request.method == "GET":
+        dealer_reviews = get_dealer_reviews_from_cf(dealer_id)
+        context = {
+            "dealer_id": dealer_id,
+            "reviews": dealer_reviews
+        }
+        dealer_details = render(
+            request, 'djangoapp/reviews.html', context)
+    return dealer_details
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_dealer_review(request, dealer_id):
+    """ Add Review View """
+    if request.method == "GET":
+        cars = CarModel.objects.filter(dealer_id=dealer_id)
+        context = {"cars": cars, "dealer_id": dealer_id}
+        add_review_view = render(request, 'djangoapp/add_review.html', context)
+    if request.method == "POST" and request.user.is_authenticated:
+        form = request.POST
+        review = {
+            "review_id": random.randint(0, 100),
+            "reviewer_name": form["fullname"],
+            "dealership": dealer_id,
+            "review": form["review"]
+        }
+        if form.get("purchase"):
+            review["purchase"] = True
+            review["purchase_date"] = form["purchasedate"]
+            car = get_object_or_404(CarModel, pk=form["car"])
+            review["car_make"] = car.carmake.name
+            review["car_model"] = car.name
+            review["car_year"] = car.year
+        json_result = add_dealer_review_to_db(review)
+        add_review_view = redirect(
+            'djangoapp:dealer_reviews', dealer_id=dealer_id)
+    return add_review_view
