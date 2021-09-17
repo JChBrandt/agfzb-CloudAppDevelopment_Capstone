b738715b50cb01c75947c70749d6e54d1c840876
JChBrandt <59009010+JChBrandt@users.noreply.github.com>
2021-09-17 19:01:33 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,14 +13,24 @@ from .local_params_settings import COUCH_URL, COUCH_USERNAME, IAM_API_KEY
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
     try:
-        response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
+        response = {}
+        if api_key:
+            response = requests.get(
+                url, 
+                headers={'Content-Type': 'application/json'}, 
+                auth=HTTPBasicAuth('apikey', api_key),
+                params=kwargs)
+        else:
+            response = requests.get(
+                url, 
+                headers={'Content-Type': 'application/json'}, 
+                params=kwargs)
         json_data = json.loads(response.text)
         return json_data
     except:
         # If any error occurs
         print("Network exception occurred")
 #    status_code = response.status_code
-
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
@@ -65,18 +75,31 @@ def get_dealer_reviews_from_cf(url, dealerId):
         for anyreview in reviews:
             sentiment = "sentiment"
 #            sentiment = analyze_review_sentiments(anyreview["review"])
-            review = DealerReview(
-                id=anyreview["id"],
-                name=anyreview["name"],
-                dealership=anyreview["dealership"],
-                review=anyreview["review"],
-                purchase=anyreview["purchase"],
-                car_make=anyreview["car_make"],
-                car_model=anyreview["car_model"],
-                car_year=anyreview["car_year"],
-                purchase_date=anyreview["purchase_date"],
-                sentiment=sentiment
-            )
+            review = {}
+            if anyreview["purchase"]:
+                review = DealerReview(
+                  id=anyreview["id"],
+                  name=anyreview["name"],
+                  dealership=anyreview["dealership"],
+                  review=anyreview["review"],
+                  purchase=anyreview["purchase"],
+                  car_make=anyreview["car_make"],
+                  car_model=anyreview["car_model"],
+                  car_year=anyreview["car_year"],
+                  purchase_date=anyreview["purchase_date"],
+                  sentiment=sentiment)
+            else:
+                review = DealerReview(
+                  id=anyreview["id"],
+                  name=anyreview["name"],
+                  dealership=anyreview["dealership"],
+                  review=anyreview["review"],
+                  purchase=anyreview["purchase"],
+                  car_make=None,
+                  car_model=None,
+                  car_year=None,
+                  purchase_date=None,
+                  sentiment=None)                   
             results.append(review)
     return results
 
@@ -96,6 +119,6 @@ def add_dealer_review(review_to_post):
     return post_request(API_URL_REVIEW, review)
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
+def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
